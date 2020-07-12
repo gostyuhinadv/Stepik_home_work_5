7a3f52314fa0db64ffc1f9ed587cecbdbd9172a7
gostyuhinadv <spessod@yandex.ru>
2020-07-12 17:41:45 +0700

    удалена библиотека time

--- test_make_report.py
+++ test_make_report.py
@@ -1,13 +1,12 @@
-from time import sleep
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_register(browser):
     browser.get(link)
     browser.find_element_by_css_selector('#login_link').click()
-    browser.find_element_by_css_selector('[name="registration-email"]').send_keys("aregh111qa@ya.ru")
+    browser.find_element_by_css_selector('[name="registration-email"]').send_keys("aeppxvbg@ya.ru")
     browser.find_element_by_css_selector('[name="registration-password1"]').send_keys("sjuekju1432l")
     browser.find_element_by_css_selector('[name="registration-password2"]').send_keys("sjuekju1432l")
     browser.find_element_by_css_selector('[name="registration_submit"]').click()
     greeting = browser.find_element_by_css_selector(".alertinner.wicon")
-    assert greeting != None, "greeting not found!"
+    assert greeting != None , "greeting not found!"
