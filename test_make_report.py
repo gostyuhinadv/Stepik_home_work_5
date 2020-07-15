b5d78d5fba0f9682619d47fcb9f3546e973f9c61
gostyuhinadv <spessod@yandex.ru>
2020-07-15 22:01:52 +0700

    Работа над ошибками

--- test_make_report.py
+++ test_make_report.py
@@ -4,9 +4,9 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_register(browser):
     browser.get(link)
     browser.find_element_by_css_selector('#login_link').click()
-    browser.find_element_by_css_selector('[name="registration-email"]').send_keys("aeppxvbg@ya.ru")
+    browser.find_element_by_css_selector('[name="registration-email"]').send_keys("ae1piinnkxjg@ya.ru")
     browser.find_element_by_css_selector('[name="registration-password1"]').send_keys("sjuekju1432l")
     browser.find_element_by_css_selector('[name="registration-password2"]').send_keys("sjuekju1432l")
     browser.find_element_by_css_selector('[name="registration_submit"]').click()
     greeting = browser.find_element_by_css_selector(".alertinner.wicon")
-    assert greeting != None , "greeting not found!"
+    assert greeting.text == "Thanks for registering!" , "greeting not found!"
